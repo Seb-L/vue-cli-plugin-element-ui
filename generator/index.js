efbc7513c0ec3e8ffbd82eb5ddae625f3db95449
Seb.L <sebastien.lombard75@gmail.com>
2018-04-21 23:27:21 +0200

    i18n strategy

--- generator/index.js
+++ generator/index.js
@@ -1,4 +1,4 @@
-module.exports = api => {
+module.exports = (api, opts) => {
   api.extendPackage({
     dependencies: {
       'element-ui': '^2.3.6'
@@ -6,6 +6,7 @@ module.exports = api => {
   })
 
   api.postProcessFiles(files => {
+    // update main file
     const file = files['src/main.ts']
       ? 'src/main.ts'
       : 'src/main.js'
@@ -16,12 +17,48 @@ module.exports = api => {
       const lastImportIndex = lines.findIndex(line => line.match(/^import/))
 
       lines[lastImportIndex] += `\nimport ElementUI from 'element-ui'`
-      lines[lastImportIndex] += `\nimport locale from 'element-ui/lib/locale/lang/en'`
+
+      if (opts.standardLocale) {
+        lines[lastImportIndex] += `\nimport locale from 'element-ui/lib/locale/lang/${opts.standardLocale}'`
+      }
+
       lines[lastImportIndex] += `\nimport 'element-ui/lib/theme-chalk/index.css'`
       lines[lastImportIndex] += `\n`
-      lines[lastImportIndex] += `\nVue.use(ElementUI, { locale })`
+
+      if (opts.standardLocale) {
+        lines[lastImportIndex] += `\nVue.use(ElementUI, { locale })`
+      } else {
+        lines[lastImportIndex] += `\nVue.use(ElementUI)`
+      }
 
       files[file] = lines.reverse().join('\n')
+    }
+
+    // update i18n file
+    if (opts.vuei18nLocales.length) {
+      const i18nFile = files['src/i18n.ts']
+        ? 'src/i18n.ts'
+        : 'src/i18n.js'
+      const i18n = files[i18nFile]
+      if (i18n) {
+        // inject import for registerServiceWorker script into main.js
+        const i18nLines = i18n.split(/\r?\n/g).reverse()
+        const i18nLastImportIndex = i18nLines.findIndex(line => line.match(/^import/))
+
+        opts.vuei18nLocales.forEach(locale => {
+          i18nLines[i18nLastImportIndex] += `\nimport ${locale.replace('-', '_')} from 'element-ui/lib/locale/lang/${locale}'`
+        })
+
+        // message obj init
+        const msgObjIndex = i18nLines.findIndex(line => line.match(/^\s\sconst messages = {}/))
+        i18nLines[msgObjIndex] = `  const messages = { ${opts.vuei18nLocales.join(', ').replace(/-/gm, '_')} }`
+
+        // message concat
+        const msgConcatIndex = i18nLines.findIndex(line => line.match(/^\s\s\s\smessages\[locale\] = locales\(key\)/))
+        i18nLines[msgConcatIndex] = `    messages[locale] = Object.assign(messages, locales(key))`
+
+        files[i18nFile] = i18nLines.reverse().join('\n')
+      }
     }
   })
 }
---
+++ prompts.js
@@ -0,0 +1,30 @@
+const localeList = ['af-ZA', 'ar', 'bg', 'ca', 'cs-CZ', 'da', 'de', 'ee', 'el', 'en', 'es', 'fa', 'fi', 'fr', 'he', 'hu', 'id', 'it', 'ja', 'ko', 'ku', 'kz', 'lt', 'lv', 'mn', 'nb-NO', 'nl', 'pl', 'pt-br', 'pt', 'ro', 'ru-RU', 'sk', 'sl', 'sv-SE', 'ta', 'th', 'tk', 'tr-TR', 'ua', 'ug-CN', 'vi', 'zh-CN', 'zh-TW']
+
+module.exports = [
+  {
+    type: 'list',
+    name: 'i18n',
+    message: 'ElementUi i18n options',
+    choices: [
+      { name: 'None', value: 'none' },
+      { name: 'Standard', value: 'standard' },
+      { name: 'VueI18n (with i18n cli plugin)', value: 'vuei18n' }
+    ],
+    default: 'None',
+  },
+  {
+    when: answers => answers.i18n === 'standard',
+    type: 'list',
+    name: 'standardLocale',
+    message: 'Select the locale you want to load',
+    choices: localeList,
+    default: 'en',
+  },
+  {
+    when: answers => answers.i18n === 'vuei18n',
+    type: 'checkbox',
+    name: 'vuei18nLocales',
+    message: 'Select the locales you want to load',
+    choices: localeList
+  }
+]
